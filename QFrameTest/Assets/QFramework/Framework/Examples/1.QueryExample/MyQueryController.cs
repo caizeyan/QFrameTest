03aab9e6ef3afb55cc26fd1064b07e35b0859200
蔡泽言 <zy_cai@droidhang.com>
2022-11-28 11:35:43 +0800

    Query介绍  Controller向Model发送的请求  Command的用于增 删 改  Query用于查

---
+++ QFrameTest/Assets/QFramework/Framework/Examples/1.QueryExample/MyQueryController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace QFramework.MyExample
+{
+    public class MyQueryController : MonoBehaviour,IController
+    {
+        public class StudentModel:AbstractModel
+        {
+            public List<string> StudentNames = new List<string>()
+            {
+                "张三",
+                "李四",
+            };
+            protected override void OnInit()
+            {
+                
+            }
+        }
+        
+        public class TeacherModel:AbstractModel
+        {
+            public List<string> TeacherNames = new List<string>()
+            {
+                "王五",
+                "赵六"
+            };
+            protected override void OnInit()
+            {
+                
+            }
+        }
+
+        public class QueryApp : Architecture<QueryApp>
+        {
+            protected override void Init()
+            {
+                this.RegisterModel(new StudentModel());
+                this.RegisterModel(new TeacherModel());
+            }
+        }
+        
+        public class SchoolAllPersonCountQuery : AbstractQuery<int>
+        {
+            protected override int OnDo()
+            {
+                return this.GetModel<StudentModel>().StudentNames.Count +
+                       this.GetModel<TeacherModel>().TeacherNames.Count;
+            }
+        }
+
+        private int allPersonCount = 0;
+
+        private void OnGUI()
+        {
+            GUILayout.Label(allPersonCount.ToString());
+            if (GUILayout.Button("查询学校总人数"))
+            {
+                allPersonCount = this.SendQuery(new SchoolAllPersonCountQuery());
+            }
+        }
+
+        public IArchitecture GetArchitecture()
+        {
+            return QueryApp.Interface;
+        }
+    }    
+}
+
